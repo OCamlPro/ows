6ee5a1163df884f9b010b89f90d23a5942582940
Pietro Abate <pietro.abate@pps.jussieu.fr>
2015-03-24 00:58:06 +0100

    Refactoring
    - move js code in ows.js
    - add command line to ows-archive

---
+++ js/ows.js
@@ -0,0 +1,74 @@
+    // populate the modal with the content of the href
+    $('#myModal').on('loaded.bs.modal', function (e) {
+	    var table = $(this).find('#modal-table');
+        table.DataTable({
+		  "bInfo" : false,
+		  "retrieve": true,
+		//  "scrollY": "250px",
+		//  "scrollCollapse": true,
+		  "paging": false,
+		//  "jQueryUI": true,
+		  "bFilter": false,
+		  "ordering": false,
+        });
+		//table.columns.adjust().draw();
+		var m = $(this);
+		$(this).find("#modal-tab a").click(function(e){
+		  e.preventDefault();
+		  var href = $(this).attr("href");
+		  $(m).find(".collapse.in").collapse('hide');
+		  $(this).addClass('active');
+		  $(m).find(href).tab().show();
+		});
+		$(this).find('.reasons').click(function(e){
+		  e.preventDefault();
+		  var href = $(this).attr("href");
+		  $(m).find('#overview').tab().hide();
+		  $(m).find("#modal-tab a[href='#details']").tab('show');
+		  $(m).find(".collapse.in").collapse('hide');
+		  $(m).find(href).collapse('toggle');
+		});
+		$(m).find('.package-backlink').click(function(e){
+		  e.preventDefault();
+		  var p = $(this).attr("href");
+		  m.removeData('bs.modal');
+		  page = window.location.pathname.split( '/' ).slice(-1)[0];
+		  if (page != 'index.html') {
+			// from the summary page
+			window.location = 'index.html'+p;
+		  }
+		  else
+			document.getElementById(p).click();
+		});
+		$(m).find('.version-backlink').click(function(e){
+		  e.preventDefault();
+		  var p = $(this).attr("href");
+		  var v = $(this).attr("version");
+		  // this does not do anything different from above. it should
+		  // select a specific version in the modal table
+		  m.removeData('bs.modal');
+		  document.getElementById(p).click();
+		});
+    });
+
+    // destroy the modal object when closed
+	$('body').on('hidden.bs.modal', '.modal', function () {
+		$(this).removeData('bs.modal');
+	});
+
+	$(document).ready(function() {
+	    // init the dataTables object in the main page
+		$('#package-table').DataTable( {
+		  "stateSave": true,
+		  "ordering": false,
+		  "paging": false,
+		} );
+		$('#summary-table').DataTable( {
+		  "ordering": false,
+		  "stateSave": true,
+		  "paging": false,
+		} );
+		if (window.location.hash)
+		  document.getElementById(window.location.hash).click();
+	} );
+
