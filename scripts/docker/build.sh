7ffabc6501ea5549e00b12388c2b12a3bfefa4a5
Pietro Abate <pietro.abate@pps.jussieu.fr>
2015-06-05 18:49:32 +0200

    Docker file work in progress

--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 cat <<EOF > ows.conf
-BASEDIR=/repos/ows
+BASEDIR=/home/ows/ows
 VERSIONS="${VERSIONS:-"4.02.1 4.01.0 4.02.0 4.00.1 3.12.1"}"
 
-DISTCHECK=/dose/distcheck.native
+DISTCHECK=/home/ows/dose/distcheck.native
 
-OPAM=/opam/src/opam
+OPAM=/home/ows/opam/src/opam
 OPAMOPTIONS="--use-internal-solver"
 
-OWSRUN=/ows/ows-run
-OWSARCHIVE=/ows/ows-archive
-OWSUPDATE=/ows/ows-update
+OWSRUN=/home/ows/ows/ows-run
+OWSARCHIVE=/home/ows/ows/ows-archive
+OWSUPDATE=/home/ows/ows/ows-update
 
 REPORTDIR=${BASEDIR}/reports
 
 DATADIR=${BASEDIR}/repository
 
-TMPDIR=/tmp
+TMPDIR=/home/ows/tmp
 
 TARGETDIR=${BASEDIR}/html
 
@@ -29,10 +29,12 @@ cat <<EOF > Dockerfile
 FROM debian:jessie
 MAINTAINER Pietro Abate <pietro.abate@pps.univ-paris-diderot.fr>
 
-RUN echo "deb http://ftp.fr.debian.org/debian unstable main" > /etc/apt/sources.list
+RUN echo "deb http://ftp.ens-cachan.fr/ftp/debian/ unstable main" > /etc/apt/sources.list
 RUN apt-get update
 
 RUN apt-get install --no-install-recommends --yes \
+  vim \
+  less \
   build-essential \
   unzip \
   rsync \
@@ -52,20 +54,24 @@ RUN apt-get install --no-install-recommends --yes \
   libbz2-ocaml-dev \
   libre-ocaml-dev
 
-ADD gitclone.sh /$GITCLONE
-RUN ./$GITCLONE
+RUN useradd -c 'ows' -m -d /home/ows -s /bin/bash ows
+USER ows
+ENV HOME /home/ows
+
+ADD gitclone.sh /home/ows/$GITCLONE
+RUN cd /home/ows && ./$GITCLONE
 #RUN git config --global http.sslVerify "false"
 #RUN git clone https://scm.gforge.inria.fr/anonscm/git/dose/dose.git 
 #RUN git clone https://github.com/OCaml/opam.git 
 #RUN git clone https://github.com/OCamlPro/ows.git 
 
-RUN cd opam && ./configure && make lib-ext all
-RUN cd dose && ./configure && make
+RUN cd /home/ows/opam && ./configure && make lib-ext all
+RUN cd /home/ows/dose && ./configure && make
 
-ADD ows.conf /
-RUN cd ows && DEFAULTS=/ows.conf ./ows-update -s
-RUN ls -la /repository
-RUN cd ows && DEFAULTS=/ows.conf scripts/ows-cron replay 10
+ADD ows.conf /home/ows/ows
+RUN cd /home/ows/ows && DEFAULTS=/home/ows/ows/ows.conf ./ows-update -s
+RUN ls -la /home/ows/repository
+RUN cd /home/ows/ows && DEFAULTS=/ows.conf scripts/ows-cron replay 10
 EOF
 
-docker build .
+docker build -t ows-devel --no-cache . 
